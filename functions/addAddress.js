b2040f85c4b250fd90c600ca8db8d11230f68c5e
Pankaj Patel <pankaj.patel@billie.io>
2021-01-12 22:49:33 +0100

    endpoints for POST

---
+++ functions/addAddress.js
@@ -0,0 +1,15 @@
+exports.handler = function (event, context, callback) {
+  if (event.httpMethod !== "POST") {
+    return {
+      statusCode: 401,
+      body: JSON.stringify({ status: "Error", message: "Not allowed" }),
+    };
+  }
+
+  const reqBody = JSON.parse(event.body);
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(Object.assign(reqBody, {__endpoint: 'addAddress', receivedAt: +new Date()}))
+  }
+}
